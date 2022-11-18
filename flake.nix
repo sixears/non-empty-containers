09e61542c2a30eb43f1cfd0e7210383e71fc2a5b
Mr. Fluffy <git@sixearsm>
2022-11-18 18:08:00 +0000

    has-callstack->1.0.1.13

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "Containers that may not be empty, by construction";
 
   inputs = {
-    nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.12";
+    nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+#    build-utils.url   = github:sixears/flake-build-utils/r1.0.0.12;
+    build-utils.url   = path:/home/martyn/src/flake-build-utils;
 
-    base1.url         = "github:sixears/base1/r0.0.9.20";
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.9";
-    tasty-plus.url    = "github:sixears/tasty-plus/r1.5.2.16";
+    base1.url         = github:sixears/base1/r0.0.9.22;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.9;
+    tasty-plus.url    = github:sixears/tasty-plus/r1.5.2.16;
   };
 
   outputs = { self, nixpkgs, build-utils
