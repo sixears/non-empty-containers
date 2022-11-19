5f3156b03bee60a4185facbe4a78206904d69a23
Mr. Fluffy <git@sixearsm>
2022-11-19 09:59:10 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
 
   inputs = {
     nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
-#    build-utils.url   = github:sixears/flake-build-utils/r1.0.0.12;
-    build-utils.url   = path:/home/martyn/src/flake-build-utils;
+    build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
     base1.url         = github:sixears/base1/r0.0.9.31;
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
@@ -14,9 +13,26 @@
   outputs = { self, nixpkgs, build-utils
             , base1, more-unicode, tasty-plus }:
     build-utils.lib.hOutputs self nixpkgs "non-empty-containers" {
-      deps = {
-        inherit base1 more-unicode tasty-plus;
-      };
       ghc = p: p.ghc8107; # for tfmt
+      callPackage = { mkDerivation, lib, mapPkg, system
+                    , base, base-unicode-symbols, containers, deepseq, lens
+                    , mono-traversable, QuickCheck, tasty, tasty-hunit
+                    , tasty-quickcheck, template-haskell, text, text-printer
+                    , unordered-containers
+                    }:
+        mkDerivation {
+          pname = "non-empty-containers";
+          version = "1.4.3.33";
+          src = ./.;
+          libraryHaskellDepends = [
+            base base-unicode-symbols containers deepseq lens mono-traversable
+            QuickCheck tasty tasty-hunit tasty-quickcheck template-haskell text
+            text-printer
+            unordered-containers
+          ] ++ mapPkg [ base1 more-unicode tasty-plus ];
+          testHaskellDepends = [ base tasty ];
+          description = "Containers that may not be empty, by construction";
+          license = lib.licenses.mit;
+        };
     };
 }
