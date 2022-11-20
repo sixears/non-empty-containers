35fdcfb98ff484b8fa6a3ed868f8f3b555181e6d
Mr. Fluffy <git@sixearsm>
2022-11-20 06:53:35 +0000

    exited->1.0.4.22;monaderror-io->1.2.5.19;tfmt->0.2.7.24;number->1.1.2.14;has-callstack->1.0.1.18;base0->0.0.4.11;more-unicode->0.0.17.12;quasiquoting->1.0.1.31

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
-    base1.url         = github:sixears/base1/r0.0.9.32;
-    more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
-    tasty-plus.url    = github:sixears/tasty-plus/r1.5.2.22;
+    base1.url         = github:sixears/base1/r0.0.9.33;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.12;
+    tasty-plus.url    = github:sixears/tasty-plus/r1.5.2.23;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -22,7 +22,7 @@
                     }:
         mkDerivation {
           pname = "non-empty-containers";
-          version = "1.4.3.34";
+          version = "1.4.3.35";
           src = ./.;
           libraryHaskellDepends = [
             base base-unicode-symbols containers deepseq lens mono-traversable
